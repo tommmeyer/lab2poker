97a81f8d1313a55129486c765f2e6def07377f71
tommeyer <tommeyer@toms-mbp.home>
2016-09-25 15:01:21 -0400

    more junit test updates

--- RioBLL/src/test/java/pokerBase/HandTest.java
+++ RioBLL/src/test/java/pokerBase/HandTest.java
@@ -37,6 +37,14 @@ public class HandTest {
 		Hand royalFlush = new Hand();
 		royalFlush.setHand(cardOne,cardTwo,cardThree,cardFour,cardFive);
 		assertTrue(Hand.isHandRoyalFlush(royalFlush));
+		Card cardone = new Card(1,0,1);
+		Card cardtwo = new Card(2,0,2);
+		Card cardthree = new Card(3,0,3);
+		Card cardfour = new Card(0,0,4);
+		Card cardfive = new Card(12,0,5);
+		Hand straightFlush = new Hand();
+		straightFlush.setHand(cardone,cardtwo,cardthree,cardfour,cardfive);
+		assertFalse(Hand.isHandRoyalFlush(straightFlush));
 		
 	}
 
@@ -50,6 +58,14 @@ public class HandTest {
 		Hand fourOfAKind = new Hand();
 		fourOfAKind.setHand(cardOne,cardTwo,cardThree,cardFour,cardFive);
 		assertTrue(Hand.isHandFourOfAKind(fourOfAKind));
+		Card cardone = new Card(11,0,1);
+		Card cardtwo = new Card(10,0,2);
+		Card cardthree = new Card(9,0,3);
+		Card cardfour = new Card(8,0,4);
+		Card cardfive = new Card(12,0,5);
+		Hand royalFlush = new Hand();
+		royalFlush.setHand(cardone,cardtwo,cardthree,cardfour,cardfive);
+		assertFalse(Hand.isHandFourOfAKind(royalFlush));
 	}
 
 	@Test
@@ -62,6 +78,14 @@ public class HandTest {
 		Hand straightFlush = new Hand();
 		straightFlush.setHand(cardOne,cardTwo,cardThree,cardFour,cardFive);
 		assertTrue(Hand.isHandStraightFlush(straightFlush));
+		Card cardone = new Card(11,0,1);
+		Card cardtwo = new Card(10,0,2);
+		Card cardthree = new Card(9,0,3);
+		Card cardfour = new Card(8,0,4);
+		Card cardfive = new Card(12,0,5);
+		Hand royalFlush = new Hand();
+		royalFlush.setHand(cardone,cardtwo,cardthree,cardfour,cardfive);
+		assertFalse(Hand.isHandStraightFlush(royalFlush));
 	}
 	
 }
